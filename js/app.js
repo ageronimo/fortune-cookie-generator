d69b13c38117a4c770999304d5687440d711cb97
Angela Geronimo <angelageroniimo@gmail.com>
2016-12-23 23:43:24 -1000

    im tired and idk what im doing

--- js/app.js
+++ js/app.js
@@ -4,13 +4,18 @@ var fortunesList = ["You will have a great day.", "Someone will say 'hi' to you.
 var i;
 
 function generateFortuneCookie() {
+  /* original code
   var node = document.createElement("LI");
   var textnode = document.createTextNode(fortunesList);
   node.appendChild(textnode);
-  node.getElementById("fortune-cookie-text").appendChild(node);
+  node.getElementById("fortune-cookie-text").appendChild(node);*/
+  
+  var fortune = document.getElementById(fortune-cookie-text);
+  var list = document.getElementById("list");
+  var li = document.createElement("li");
 
-  for (i = 0; i < fortunesList.length; i++) {
-    console.log
-  }
+  fortune.innerHTML = fortunesList[Math.floor(Math.random()*fortunesList.length)];;
 
+  li.appendChild(document.createTextNode(fortune.textContent));
+  list.appendChild(li);
 }
