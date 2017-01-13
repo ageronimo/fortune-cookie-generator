3658aa4a272baecc4a6370bd593440704c4da550
ageronimo <angelageroniimo@gmail.com>
2017-01-13 09:22:17 -1000

    DONE

--- js/app.js
+++ js/app.js
@@ -2,25 +2,20 @@
 // list of fortunes
 var fortunesList = ["You will have a great day.", "Someone will say 'hi' to you.", "You're going to trip!", "I see a chilly gust in your future."];
 var i;
+var targetFortune = [];
 
 function generateFortuneCookie() {
   //use appendChild() method to make an list
   var li = document.createElement("li"); //creates an li node 
-  var fortune = document.createTextNode(fortunesList); //creates a text node 
-  li.appendChild(fortune); //appends text to li (makes li -> text), fortune becomes child
-  document.getElementById("fortune-cookie-text").appendChild(li); //attaches node to list
+  var fortunes = fortunesList[Math.floor(Math.random()*fortunesList.length)];
+  document.getElementById("fortune-cookie-text").innerText = fortunes;
+  var fortune = document.createTextNode(fortunes); //creates a text node 
+  li.appendChild(fortune);
   document.getElementById("list").appendChild(li);
   
-  //showing the actual fortune
-  var fortune = fortunesList[Math.floor(Math.random()*fortunesList.length)];
-  document.getElementById("fortune-cookie-text").innerText = fortune;
+  console.log(test);
 
   //target single fortunes
-  var targetFortune = document.getElementById("list").getElementsByTagName("li");
-  if (fortune == fortunesList[0]){
-    targetFortune = fortune;
-  } else if (fortune == fortunesList[1]){
-    targetFortune = fortune;
-  }
-  console.log(targetFortune);
+  targetFortune.push(fortune);
+  document.getElementById("list").innerText = targetFortune;
 }
